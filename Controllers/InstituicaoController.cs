4437d715be5151920d8d62fae09e49d35c44cf11
MATEUS VIZOTO <mateus.vizoto@fatec.sp.gov.br>
2022-05-13 17:28:10 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/InstituicaoController.cs
@@ -0,0 +1,80 @@
+﻿using Cap01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cap01.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>(){
+            new Instituicao(){InstituicaoID = 1,
+                Nome = "Fatec",
+                Endereco = "Taquaritinga"
+
+            },
+            new Instituicao(){InstituicaoID = 2,
+                Nome = "Itis",
+                Endereco = "Taquaritinga",
+            },
+            new Instituicao(){InstituicaoID = 3,
+                Nome = "Santa Ursula",
+                Endereco = "Araraquara",
+            },
+            new Instituicao(){InstituicaoID = 4,
+                Nome =  "Santa Monica",
+                Endereco = "São Carlos",
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(instituicoes.OrderBy(i => i.Nome));
+        }
+        /*creat abaixo*/
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Instituicao instituicao)
+        {
+            instituicoes.Add(instituicao);
+            instituicao.InstituicaoID = instituicoes.Select(i =>  i.InstituicaoID).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
+        /*edit abaixo*
+        public IActionResult Edit(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
+            instituicoes.Add(instituicao);
+            return RedirectToAction("Index");
+        }*/
+
+        /*Detalhes Abaixo
+        public ActionResult Details(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        /* Delete abaixo*/
+        public ActionResult Delete(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Instituicao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cap01.Models
+{
+    public class Instituicao
+    {
+        public long? InstituicaoID { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+
+        internal object Select(Func<object, object> p)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Add(Instituicao instituicao)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
