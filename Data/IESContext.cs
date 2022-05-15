dac0d3784ce34a76ef5135e02c945cd6224722cf
MATEUS VIZOTO <mateus.vizoto@fatec.sp.gov.br>
2022-05-15 08:38:17 -0300

    addDb

---
+++ Data/IESContext.cs
@@ -0,0 +1,17 @@
+﻿using Cap01.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Cap01.Data
+{
+    public class IESContext : DbContext
+    {
+        public IESContext(DbContextOptions<IESContext> options) : base(options) 
+        {
+        }
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Instituicao> Instituicoes{ get; set; }
+
+    }
+
+}
